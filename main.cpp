2b50b776233398f48c9fb33a058e86a9fd15a28e
Wei-Tse Sun <wtse.sun@gmail.com>
2022-07-15 20:18:35 -0700

    create a terminal clock

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <stdlib.h>
+
+using namespace std;
+string getTime()
+{
+  std::time_t t = std::time(0);
+  std::tm *now = std::localtime(&t);
+  return to_string((now->tm_hour)) + ':' + to_string((now->tm_min + 1)) + ':' + to_string((now->tm_sec));
+}
+int main()
+{
+  while(1){
+    system("clear");
+    cout << getTime() << endl;
+    std::chrono::milliseconds timespan(1000);
+    std::this_thread::sleep_for(timespan);
+  }
+}
